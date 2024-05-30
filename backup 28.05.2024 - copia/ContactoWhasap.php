d48095f8d4c8fc03451296988c5851c41e5981d0
ProgresoTec <progresotec@gmail.com>
2024-05-30 01:31:36 -0500

    Delete backup 28.05.2024 - copia directory

--- backup 28.05.2024 - copia/ContactoWhasap.php
+++
@@ -1,3 +0,0 @@
-<a href="#" class="btn btn-lg btn-primary rounded-circle back-to-top" style="width: 60px; height: 60px; line-height: 70px; display: flex; justify-content: center; align-items: center;">
-        <i class="fab fa-whatsapp" style="font-size: 40px;"></i>
-    </a>
--- backup 28.05.2024 - copia/TecMarket/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "scanmarket";
-$port = "3306"
-?>
