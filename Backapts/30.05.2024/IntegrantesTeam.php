bf6dd55f9847a941ef0428b2a3ff51bafc1e223d
ProgresoTec <progresotec@gmail.com>
2024-05-30 01:41:11 -0500

    Update IntegrantesTeam.php

--- Backapts/30.05.2024/IntegrantesTeam.php
+++ Backapts/30.05.2024/IntegrantesTeam.php
@@ -5,24 +5,7 @@
             <h1 class="mb-0">Profesionales Listos para Ayudar a Tu Negocio</h1>
         </div>
         <div class="row g-5">
-            <div class="col-lg-4 wow slideInUp" data-wow-delay="0.3s">
-                <div class="team-item bg-light rounded overflow-hidden">
-                    <div class="team-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="https://via.placeholder.com/500" style="object-fit: cover;" alt="Team Member 1">
-                        <div class="team-social">
-                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
-                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
-                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
-                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-center py-4">
-                        <h4 class="text-primary">Nombre Completo</h4>
-                        <p class="text-uppercase m-0">Cargo</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 wow slideInUp" data-wow-delay="0.6s">
+            <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
                 <div class="team-item bg-light rounded overflow-hidden">
                     <div class="team-img position-relative overflow-hidden">
                         <img class="img-fluid w-100" src="img/Perfil.jpg"  alt="">
@@ -35,7 +18,60 @@
                     </div>
                     <div class="text-center py-4">
                         <h4 class="text-primary">Pierre Agustin Ramos Huaracha</h4>
-                        <p class="text-uppercase m-0">CEO - Desarrollador</p>
+                        <p class="text-uppercase m-0">Director de Tecnología - Director de Desarrollo de Software</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
+                <div class="team-item bg-light rounded overflow-hidden">
+                    <div class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="img/perfilmeme.jpg"  alt="">
+                        <div class="team-social">
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <h4 class="text-primary">Fiorella Marife Estrada Vargas</h4>
+                        <p class="text-uppercase m-0">Especialista en Atención al Cliente - Representante de Ventas</p>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
+                <div class="team-item bg-light rounded overflow-hidden">
+                    <div class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="https://via.placeholder.com/500" style="object-fit: cover;" alt="Team Member 1">
+                        <div class="team-social">
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <h4 class="text-primary">Pierre Luiggi Ramirez</h4>
+                        <p class="text-uppercase m-0">Gerente de Proyectos - Desarrollador de Software</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
+                <div class="team-item bg-light rounded overflow-hidden">
+                    <div class="team-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="https://via.placeholder.com/500" style="object-fit: cover;" alt="Team Member 1">
+                        <div class="team-social">
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
+                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <h4 class="text-primary">Lindbergh Danilo Santa Cruz Ramos</h4>
+                        <p class="text-uppercase m-0">Tester de Software - Desarrollador de Software</p>
                     </div>
                 </div>
             </div>
@@ -52,24 +88,7 @@
                     </div>
                     <div class="text-center py-4">
                         <h4 class="text-primary">Nombre Completo</h4>
-                        <p class="text-uppercase m-0">Cargo</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
-                <div class="team-item bg-light rounded overflow-hidden">
-                    <div class="team-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="https://via.placeholder.com/500" style="object-fit: cover;" alt="Team Member 1">
-                        <div class="team-social">
-                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-twitter fw-normal"></i></a>
-                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
-                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-instagram fw-normal"></i></a>
-                            <a class="btn btn-lg btn-primary btn-lg-square rounded" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
-                        </div>
-                    </div>
-                    <div class="text-center py-4">
-                        <h4 class="text-primary">Nombre Completo</h4>
-                        <p class="text-uppercase m-0">Cargo</p>
+                        <p class="text-uppercase m-0">Especialista en Marketing Digital y Desarrollo de Contenido</p>
                     </div>
                 </div>
             </div>
